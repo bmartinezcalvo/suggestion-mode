e09e90891877661afb242ab0daab7b749156b6e1
bmartinezcalvo <bmartinezcalvo@wikimedia.org>
2026-01-09 14:29:50 +0100

    Configure GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/', // Base URL for Netlify (use '/' for root deployment)
+  base: '/suggestion-mode/',
 })
-
